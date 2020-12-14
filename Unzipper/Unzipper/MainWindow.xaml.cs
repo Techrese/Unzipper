e42f8c719a47c53b78494658dc512ec6fcaa289e
tim clinckemalie <tim.clinckemalie@hotmail.com>
2020-12-14 13:45:28 +0100

    Added loadfiles method

--- Unzipper/Unzipper/MainWindow.xaml.cs
+++ Unzipper/Unzipper/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace Unzipper
             InitializeComponent();
         }
 
-        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        private async void BtnBrowse_Click(object sender, RoutedEventArgs e) //the only time you may use async void
         {
             var dialog = new VistaFolderBrowserDialog();
             dialog.Description = "Select parent folder with zip files";
@@ -33,8 +34,21 @@ namespace Unzipper
             {
                 txtBrowse.Text = dialog.SelectedPath;
                 rootPath = dialog.SelectedPath;
+                await LoadFiles();
             }
         }
+
+        private async Task LoadFiles()
+        {
+            txtReport.Text += $"{DateTime.Now} loading files, this may take some time ...";
+            var zipFiles = new List<string>();
+            await Task.Run(() =>
+            {
+                zipFiles = Directory.GetFiles(rootPath, "*", SearchOption.AllDirectories).ToList();
+            });
+            txtReport.Text += $"{DateTime.Now} Found {zipFiles.Count} files";
+        }
+
 
         private void BtnClear_Click(object sender, RoutedEventArgs e)
         {
