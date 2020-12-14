caf7d198dc0d89c48af811d4e16f5607a0955d02
tim clinckemalie <tim.clinckemalie@hotmail.com>
2020-12-14 14:58:53 +0100

    Added error handling Added progress ui

--- Unzipper/Unzipper/MainWindow.xaml.cs
+++ Unzipper/Unzipper/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
-using System.Text;
+
 using System.Threading.Tasks;
 using System.Windows;
-using System.Xml.Serialization.Configuration;
+
 using Ookii.Dialogs.Wpf;
 
 
@@ -26,27 +27,59 @@ namespace Unzipper
 
         private async void BtnBrowse_Click(object sender, RoutedEventArgs e) //the only time you may use async void
         {
-            var dialog = new VistaFolderBrowserDialog();
-            dialog.Description = "Select parent folder with zip files";
-            dialog.UseDescriptionForTitle = true;
-
-            if ((bool)dialog.ShowDialog())
+            try
             {
-                txtBrowse.Text = dialog.SelectedPath;
-                rootPath = dialog.SelectedPath;
-                await LoadFiles();
+                var dialog = new VistaFolderBrowserDialog();
+                dialog.Description = "Select parent folder with zip files";
+                dialog.UseDescriptionForTitle = true;
+
+                if ((bool)dialog.ShowDialog())
+                {
+                    txtBrowse.Text = dialog.SelectedPath;
+                    rootPath = dialog.SelectedPath;
+                    await LoadFiles();
+                }
+            }
+            catch (Exception exception)
+            {
+                txtReport.Text += $"{DateTime.Now} Exception thrown {exception} \n";
+
             }
         }
 
         private async Task LoadFiles()
         {
-            txtReport.Text += $"{DateTime.Now} loading files, this may take some time ...";
-            var zipFiles = new List<string>();
-            await Task.Run(() =>
+            try
             {
-                zipFiles = Directory.GetFiles(rootPath, "*", SearchOption.AllDirectories).ToList();
-            });
-            txtReport.Text += $"{DateTime.Now} Found {zipFiles.Count} files";
+                txtReport.Text += $"{DateTime.Now} loading files, this may take some time ... \n";
+                var zipFiles = new List<string>();
+                await Task.Run(() =>
+                {
+                    zipFiles = Directory.GetFiles(rootPath, "*.zip", SearchOption.AllDirectories).ToList();
+                });
+                txtReport.Text += $"{DateTime.Now} Found {zipFiles.Count} files \n";
+                Extract(zipFiles);
+            }
+            catch (Exception exception)
+            {
+                txtReport.Text += $"{DateTime.Now} Exception thrown {exception} \n";
+            }
+        }
+
+        private void Extract(List<string> zipFiles)
+        {
+            try
+            {
+                foreach (var file in zipFiles)
+                {
+                    //extract same place create folder with same name
+                    ZipFile.ExtractToDirectory(file, file.Remove(file.Length-4,4));
+                }
+            }
+            catch (Exception exception)
+            {
+                txtReport.Text += $"{DateTime.Now} Exception thrown {exception} \n";
+            }
         }
 
 
