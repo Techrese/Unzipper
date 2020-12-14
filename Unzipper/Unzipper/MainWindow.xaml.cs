938162271816d1a9fce0ec70b858b99e599ee9b1
tim clinckemalie <tim.clinckemalie@hotmail.com>
2020-12-14 13:34:58 +0100

    Added dialog package + dialogs Added report textbox

---
+++ Unzipper/Unzipper/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization.Configuration;
+using Ookii.Dialogs.Wpf;
+
+
+
+namespace Unzipper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string rootPath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new VistaFolderBrowserDialog();
+            dialog.Description = "Select parent folder with zip files";
+            dialog.UseDescriptionForTitle = true;
+
+            if ((bool)dialog.ShowDialog())
+            {
+                txtBrowse.Text = dialog.SelectedPath;
+                rootPath = dialog.SelectedPath;
+            }
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtReport.Text = string.Empty;
+        }
+    }
+}
